bdb59b45c34ed0a9492a11e663fa29d090eff8b9
unknown <anshul.tripathi2023a@vitstudent.ac.in>
2026-04-06 10:38:54 +0530

    First Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void feedback(String feed){
+        System.out.println("Feedback submitted");
+    }
+
+    public static void main(String[] args){
+        System.out.println("Feedback submitted successfully");    
+    }
+}
+
+
